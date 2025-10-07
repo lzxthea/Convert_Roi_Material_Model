315e9e56c0495ead8f6bacc75e3fa5fba6d437cf
lzxthea <lzxthea@outlook.com>
2025-10-07 23:58:25 +0500

    feat: 修改路径;

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python3 main.py \
---train_paths='hdfs://haruna/home/byte_ad_platform/user/liuzixi.123/ecp_roi2_nobid/pid/multi_label_v1/20250918/part-0000*' \
---test_paths='hdfs://haruna/home/byte_ad_platform/user/liuzixi.123/ecp_roi2_nobid/pid/multi_label_v1/20250919/part-0000*' \
+--train_paths='hdfs://haruna/home/byte_ad_platform/user/liuzixi.123/ecp_roi2_nobid/pid_mid_anchor_model/multi_label_v1/20250918/part-0000*' \
+--test_paths='hdfs://haruna/home/byte_ad_platform/user/liuzixi.123/ecp_roi2_nobid/pid_mid_anchor_model/multi_label_v1/20250919/part-0000*' \
 --is_eval=0 \
 --is_offline=1 \
 --loss_type='weighted_cross_entropy,focal_loss' \
